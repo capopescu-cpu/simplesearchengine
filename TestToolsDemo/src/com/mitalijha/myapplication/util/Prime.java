c0d213dc98b9885fd4d8cf96e597aed139993fb2
mitali373 <mitali373@59435df2-d0cb-11de-a306-d16691e8d855>
2009-11-15 12:51:04 +0000

    Bug fixes for prime and palindrome

--- TestToolsDemo/src/com/mitalijha/myapplication/util/Prime.java
+++ TestToolsDemo/src/com/mitalijha/myapplication/util/Prime.java
@@ -11,10 +11,13 @@ public class Prime {
 	
 	public static boolean isPrime (int n)
 	{
-		for (int i=2; i<Math.sqrt(n); i++)
+		for (int i=2; i<=Math.sqrt(n); i++)
+		{	
 			if (n%i==0)
 				return false;
+				
+		}
 		return true;
-	}
+}
 	
 }
